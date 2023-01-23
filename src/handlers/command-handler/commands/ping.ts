ac557f9d14e621747101b4288bf8f31780b45894
Davi Melo <=>
2023-01-23 08:59:14 -0300

    moved ping file to a test folder

--- src/handlers/command-handler/commands/ping.ts
+++
@@ -1,9 +0,0 @@
-import { CommandInteraction } from 'discord.js';
-
-module.exports = {
-    name: 'ping',
-    description: 'pinga.',
-    run: (interaction: CommandInteraction) => {
-        interaction.reply('Pong.');
-    }
-}
