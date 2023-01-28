013d613084e15bbdc18bb6b31a2056570b3c02ec
Davi Melo <=>
2023-01-28 08:44:59 -0300

    intents fixed

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { Client } from 'discord.js';
 import eventHandler from './handlers/event-handler/eventHandler';
 
-const client = new Client({ intents: [ 7906 ]});
+const client = new Client({ intents: [ 32509  ]  });
 const { TOKEN } = process.env;
 
 (async () => {
